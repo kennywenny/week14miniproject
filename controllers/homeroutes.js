6af93ceb90074f2e7c0d89ef1daffe301cead473
Kenny Hung (he) <khung@instructors.2u.com>
2023-01-05 20:32:01 +0000

    Fetch all projects for homepage

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const Project = require('../models/Project')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const projects = await Project.findAll({
+    raw: true
+  })
+  console.log(projects)
   res.render('home')
 })
 
