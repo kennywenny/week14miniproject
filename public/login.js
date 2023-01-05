002217abb6a936adf6a8f0ab910268f43426749c
Kenny Hung (he) <khung@instructors.2u.com>
2023-01-05 20:57:15 +0000

    Create user button wired up

--- public/login.js
+++ public/login.js
@@ -3,6 +3,15 @@ loginEventHandler = async e => {
   console.log('LOGIN!')
 }
 
+createEventHandler = async e => {
+  e.preventDefault()
+  console.log('CREATE!')
+}
+
 document
   .getElementById('login_button')
   .addEventListener('click', loginEventHandler)
+
+document
+  .getElementById('create_user_button')
+  .addEventListener('click', createEventHandler)
