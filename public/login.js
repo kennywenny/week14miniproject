0958770d72870142ebff1e3717131ab857b556d3
Kenny Hung (he) <khung@instructors.2u.com>
2023-01-05 21:02:31 +0000

    Create user FE makes a request

--- public/login.js
+++ public/login.js
@@ -10,7 +10,31 @@ createEventHandler = async e => {
     .getElementById('create_user_username')
     .value
     .trim()
+  const password = document
+    .getElementById('create_user_password')
+    .value
+    .trim()
+  const response = fetch('/api/login', {
+    method: 'post',
+    body: JSON.stringify({ username, password })
+  })
+  /*
+fetch("http://example.com/api/endpoint/", {
+  method: "post",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+
+  //make sure to serialize your JSON body
+  body: JSON.stringify({
+    name: myName,
+    password: myPassword
+  })
+})
+*/
   console.log('USERNAME: ' + username)
+  console.log('PASSWORD: ' + password)
 }
 
 document
