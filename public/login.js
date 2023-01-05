69a37edbedcabcdddb179e5abc115eb12140b082
Kenny Hung (he) <khung@instructors.2u.com>
2023-01-05 20:55:46 +0000

    Wire up login button

--- public/login.js
+++ public/login.js
@@ -1,1 +1,8 @@
-console.log('Login script')
+loginEventHandler = async e => {
+  e.preventDefault()
+  console.log('LOGIN!')
+}
+
+document
+  .getElementById('login_button')
+  .addEventListener('click', loginEventHandler)
